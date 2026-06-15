8dc82171738096456407aff630ee3f2665e9c183
Xtrakari <nsrakip@gmail.com>
2026-06-15 16:58:38 +0800

    Update lineage_mh2lm.mk

--- lineage_mh2lm.mk
+++ lineage_mh2lm.mk
@@ -17,9 +17,24 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := G8X ThinQ
 
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+
 PRODUCT_GMS_CLIENTID_BASE := android-lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=mh2lm \
     BuildDesc="mh2lm-user 12 SKQ1.211103.001 223331636afae release-keys" \
     BuildFingerprint=lge/mh2lm/mh2lm:12/SKQ1.211103.001/223331636afae:user/release-keys
+
+# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
+AXION_MAINTAINER := Xtrakari
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Snapdragon_855
+
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 12,13  # Example: 50MP + 48MP
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 32  # Example: 42MP
