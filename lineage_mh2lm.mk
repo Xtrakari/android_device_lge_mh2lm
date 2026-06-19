bf540521df6285e02300b146d6b073ac2028fac2
Xtrakari <nsrakip@gmail.com>
2026-06-19 12:48:35 +0800

    Update lineage_mh2lm.mk

--- lineage_mh2lm.mk
+++ lineage_mh2lm.mk
@@ -22,4 +22,34 @@ PRODUCT_GMS_CLIENTID_BASE := android-lge
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=mh2lm \
     BuildDesc="mh2lm-user 12 SKQ1.211103.001 223331636afae release-keys" \
-    BuildFingerprint=lge/mh2lm/mh2lm:12/SKQ1.211103.001/223331636afae:user/release-keys
+    BuildFingerprint=lge/mh2lm/mh2lm:12/SKQ1.211103.001/223331636afae:user/release-keys \
+    RisingChipset="Snapdragon 855" \
+    RisingMaintainer="Xtrakari"
+
+#Rising Bringup
+
+#Battery Capacity
+RISING_BATTERY := "4000 mAh"
+
+#RISING_PACKAGE_TYPE := "VANILLA_AOSP"
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# whether to ship aperture camera, default is false
+PRODUCT_NO_CAMERA := false
+
+# Wether to ship lawnchair launcher
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+#UDFPS
+TARGET_HAS_UDFPS := true
+
+#GMS Flags
+WITH_GMS := false
+# ships core GMS components that are needed to run GMS environment
+#TARGET_CORE_GMS := true
+#Extra add-ons for GMS build, check RisingOS manifest for the list
+#TARGET_CORE_GMS_EXTRAS := false
+# Wether to ship pixel launcher and set it as default launcher
+#TARGET_DEFAULT_PIXEL_LAUNCHER := true
