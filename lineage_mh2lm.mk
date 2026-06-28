76f839fa7a6b7fc40efb88db62b7790d3b36f704
Xtrakari <nsrakip@gmail.com>
2026-06-28 09:03:32 +0800

    Update lineage_mh2lm.mk

--- lineage_mh2lm.mk
+++ lineage_mh2lm.mk
@@ -9,6 +9,8 @@ $(call inherit-product, device/lge/mh2lm/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+DERPFEST_BUILD_TYPE := Unofficial
+DERPFEST_BUILD_VARIANT := Stable
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_mh2lm
